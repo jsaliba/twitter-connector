71da99a548cb359e4f1892f83d4362deba72ab4b
Franco Bulgarelli <franco@zaubersoftware.com>
2011-07-26 12:51:29 -0300

    License

--- src/test/java/org/mule/twitter/TwitterFlowTestCase.java
+++ src/test/java/org/mule/twitter/TwitterFlowTestCase.java
@@ -1,12 +1,13 @@
-/*
- * $Id$
- * --------------------------------------------------------------------------------------
+/**
+ * Mule Twitter Cloud Connector
+ *
  * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
  *
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
+
 package org.mule.twitter;
 
 import org.mule.api.MuleMessage;
